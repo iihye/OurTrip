b6010f558e59ff24285ead5f3a9e5cc920d781fe
ssafyjam <jamsince1994@naver.com>
2023-11-15 17:03:55 +0900

    chore: intit spring boot

---
+++ BE/ourtrip/src/main/java/edu/ssafy/ourtrip/OurtripApplication.java
@@ -0,0 +1,13 @@
+package edu.ssafy.ourtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurtripApplication.class, args);
+	}
+
+}
