e1a47483c1d4152355849bf7e3497f6d93a662d1
ssafyjam <jamsince1994@naver.com>
2023-11-16 13:44:51 +0900

    feat: fe router

--- FE/ourtrip/src/router/index.js
+++ FE/ourtrip/src/router/index.js
@@ -9,6 +9,39 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
+    {
+      path: "/user",
+      name: "user",
+      component: UserView,
+      children: [
+        {
+          path: "join",
+          name: "user-join",
+          component: "",
+        },
+        {
+          path: "login",
+          name: "user-login",
+          component: "",
+        },
+        {
+          path: "find",
+          name: "user-find",
+          component: "",
+        },
+      ],
+    },
+    // {
+    //   path:"/mypage",
+    //   name:"mypage",
+    //   component:"",
+    //   children:[
+    //     {
+    //       path:"info"
+    //       name:"mypage-info"
+    //     }
+    //   ]
+    // }
   ],
 });
 
