f03fc8d517827fef24f3293f705f7b4f3e9ac2d3
ssafyjam <jamsince1994@naver.com>
2023-11-16 15:59:36 +0900

    feat: 장소 검색 기능 완성

---
+++ FE/ourtrip/src/components/place/PlaceOpen.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>place Auth Open</div>
+</template>
+
+<style scoped></style>
--- FE/ourtrip/src/router/index.js
+++ FE/ourtrip/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+import PlaceView from "@/views/PlaceView.vue";
+import PlaceLocation from "@/components/place/PlaceLocation.vue";
+import PlaceTitle from "@/components/place/PlaceTitle.vue";
+import PlaceCover from "@/components/place/PlaceCover.vue";
+import PlaceOpen from "@/components/place/PlaceOpen.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,7 +18,7 @@ const router = createRouter({
     {
       path: "/user",
       name: "user",
-      component: UserView,
+      component: "",
       redirect: { name: "" },
       children: [
         {
@@ -46,35 +52,35 @@ const router = createRouter({
       ],
     },
     {
-      path: "place",
+      path: "/place",
       name: "place",
-      component: "",
-      redirect: { name: "" },
+      component: PlaceView,
+      redirect: { name: "place-location" },
       children: [
         {
           path: "location",
           name: "place-location",
-          component: "",
+          component: PlaceLocation,
         },
         {
           path: "title",
           name: "place-title",
-          component: "",
+          component: PlaceTitle,
         },
         {
           path: "cover",
           name: "place-cover",
-          component: "",
+          component: PlaceCover,
         },
         {
           path: "open",
           name: "place-open",
-          component: "",
+          component: PlaceOpen,
         },
       ],
     },
     {
-      path: "list",
+      path: "/list",
       name: "list",
       component: "",
       redirect: { name: "" },
