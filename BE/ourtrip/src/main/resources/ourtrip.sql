30f6be3d99986ba997367f9f8fe2743c157d9d39
ssafyjam <jamsince1994@naver.com>
2023-11-16 11:45:22 +0900

    chore: database init setting sql script

---
+++ BE/ourtrip/src/main/resources/ourtrip.sql
@@ -0,0 +1,48 @@
+
+create database `ourtrip`;
+
+use `ourtrip`;
+
+create table `user`
+(
+	`user_id` varchar(255) NOT NULL PRIMARY KEY ,
+    `user_pw` varchar(255) NOT NULL,
+    `user_name` varchar(255) NOT NULL
+);
+
+
+create table `list`
+(
+	`list_no` integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `list_name` text NOT NULL,
+    `list_img` integer NOT NULL,
+    `user_id` varchar(255) NOT NULL,
+	CONSTRAINT `list_to_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
+);
+
+
+
+
+create table `share`
+(
+	`list_no` integer NOT NULL,
+    `user_id` varchar(255) NOT NULL,
+    CONSTRAINT `share_to_list_list_no_fk` FOREIGN KEY (`list_no`) REFERENCES `list` (`list_no`) ON DELETE CASCADE,
+    CONSTRAINT `share_to_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
+    
+);
+
+
+create table `place`
+(
+	`place_no` integer NOT NULL primary key AUTO_INCREMENT,
+    `place_name` text NOT NULL,
+    `place_url` varchar(255) NOT NULL,
+    `place_address_name` text NOT NULL,
+    `place_road_address_name` text NOT NULL,
+    `place_phone` text NOT NULL,
+    `place_x` integer NOT NULL,
+    `place_y` integer NOT NULL,
+    `list_no` integer NOT NULL,
+     CONSTRAINT `place_to_list_list_no_fk` FOREIGN KEY (`list_no`) REFERENCES `list` (`list_no`) ON DELETE CASCADE
+);
