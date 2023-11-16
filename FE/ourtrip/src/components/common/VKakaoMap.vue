43ba05285a7d5e0ffac2e759f58c111a415ccbe8
ssafyjam <jamsince1994@naver.com>
2023-11-16 17:50:24 +0900

    feat: location 기능만 완성

---
+++ FE/ourtrip/src/components/common/VKakaoMap.vue
@@ -0,0 +1,256 @@
+<script setup>
+import { ref, watch, onMounted } from "vue";
+
+const { VITE_APP_KAKAO_JS_KEY } = import.meta.env;
+var map;
+const positions = ref([]);
+const markers = ref([]);
+
+const props = defineProps({ stations: Array, selectStation: Object });
+
+watch(
+  () => props.stations.value,
+  () => {
+    console.log("1");
+    positions.value = [];
+    props.stations.forEach((station) => {
+      let obj = {};
+      obj.latlng = new kakao.maps.LatLng(station.y, station.x);
+      obj.title = station.place_name;
+
+      positions.value.push(obj);
+    });
+    loadMarkers();
+  },
+  { deep: true }
+);
+
+watch(
+  () => props.selectStation.value,
+  () => {
+    // 이동할 위도 경도 위치를 생성합니다
+    var moveLatLon = new kakao.maps.LatLng(
+      props.selectStation.lat,
+      props.selectStation.lng
+    );
+
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.panTo(moveLatLon);
+  },
+  { deep: true }
+);
+
+onMounted(() => {
+  if (window.kakao && window.kakao.maps) {
+    initMap();
+  } else {
+    const script = document.createElement("script");
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${VITE_APP_KAKAO_JS_KEY}&libraries=services,clusterer`;
+    /* global kakao */
+    script.onload = () => kakao.maps.load(() => initMap());
+    document.head.appendChild(script);
+  }
+});
+
+const initMap = () => {
+  const container = document.getElementById("map");
+  const options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3,
+  };
+  map = new kakao.maps.Map(container, options);
+
+  // loadMarkers();
+};
+
+const loadMarkers = () => {
+  // 현재 표시되어있는 marker들이 있다면 map에 등록된 marker를 제거한다.
+  deleteMarkers();
+
+  // 마커 이미지를 생성합니다
+  //   const imgSrc = require("@/assets/map/markerStar.png");
+  // 마커 이미지의 이미지 크기 입니다
+  //   const imgSize = new kakao.maps.Size(24, 35);
+  //   const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
+
+  // 마커를 생성합니다
+  markers.value = [];
+  console.log(positions);
+  positions.value.forEach((position) => {
+    const marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: position.latlng, // 마커를 표시할 위치
+      title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨.
+      clickable: true, // // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+      // image: markerImage, // 마커의 이미지
+    });
+    markers.value.push(marker);
+
+    // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+    kakao.maps.event.addListener(marker, "click", function () {
+      // 커스텀 오버레이에 표시할 컨텐츠 입니다
+      // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+      // 별도의 이벤트 메소드를 제공하지 않습니다
+
+      let content = `<div class="wrap">
+            <div class="info">
+                <div class="title">
+                    ${position.title}
+                    <div class="close" onclick="${closeOverlay()}" title="닫기"></div>
+                </div>" 
+                <div class="body">
+                    <div class="img">
+                        <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">
+                   </div>
+                    <div class="desc">
+                        <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>
+                        <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>
+                        <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+      // 마커 위에 커스텀오버레이를 표시합니다
+      // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+      const overlay = new kakao.maps.CustomOverlay({
+        content: content,
+        map: map,
+        position: marker.getPosition(),
+      });
+      overlay.setMap(map);
+
+      // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
+      function closeOverlay() {
+        overlay.setMap(null);
+      }
+    });
+    // 마커에 클릭이벤트를 등록합니다
+    // kakao.maps.event.addListener(marker, "click", function () {
+    //   // 마커 위에 인포윈도우를 표시합니다
+    //   console.log("click");
+    // });
+  });
+
+  // 4. 지도를 이동시켜주기
+  // 배열.reduce( (누적값, 현재값, 인덱스, 요소)=>{ return 결과값}, 초기값);
+  const bounds = positions.value.reduce(
+    (bounds, position) => bounds.extend(position.latlng),
+    new kakao.maps.LatLngBounds()
+  );
+
+  map.setBounds(bounds);
+};
+
+const deleteMarkers = () => {
+  if (markers.value.length > 0) {
+    markers.value.forEach((marker) => marker.setMap(null));
+  }
+};
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style>
+#map {
+  width: 50%;
+  height: 700px;
+}
+
+/* 오버레이를 위한 css */
+.wrap {
+  position: absolute;
+  left: 0;
+  bottom: 40px;
+  width: 288px;
+  height: 132px;
+  margin-left: -144px;
+  text-align: left;
+  overflow: hidden;
+  font-size: 12px;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  line-height: 1.5;
+}
+.wrap * {
+  padding: 0;
+  margin: 0;
+}
+.wrap .info {
+  width: 286px;
+  height: 120px;
+  border-radius: 5px;
+  border-bottom: 2px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+  background: #fff;
+}
+.wrap .info:nth-child(1) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.info .title {
+  padding: 5px 0 0 10px;
+  height: 30px;
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-size: 18px;
+  font-weight: bold;
+}
+.info .close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #888;
+  width: 17px;
+  height: 17px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png");
+}
+.info .close:hover {
+  cursor: pointer;
+}
+.info .body {
+  position: relative;
+  overflow: hidden;
+}
+.info .desc {
+  position: relative;
+  margin: 13px 0 0 90px;
+  height: 75px;
+}
+.desc .ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.desc .jibun {
+  font-size: 11px;
+  color: #888;
+  margin-top: -2px;
+}
+.info .img {
+  position: absolute;
+  top: 6px;
+  left: 5px;
+  width: 73px;
+  height: 71px;
+  border: 1px solid #ddd;
+  color: #888;
+  overflow: hidden;
+}
+.info:after {
+  content: "";
+  position: absolute;
+  margin-left: -12px;
+  left: 50%;
+  bottom: 0;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.info .link {
+  color: #5085bb;
+}
+</style>
--- FE/ourtrip/src/components/place/PlaceLocation.vue
+++ FE/ourtrip/src/components/place/PlaceLocation.vue
@@ -1,11 +1,16 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import VKakaoMap from "../common/VKakaoMap.vue";
+
 const { VITE_APP_KAKAO_API_URI, VITE_APP_KAKAO_REST_KEY } = import.meta.env;
 
 const searchKeyword = ref("");
 const searchList = ref([]);
-const searchHandeler = async () => {
+const selectList = ref([]);
+const selectPlace = ref({});
+
+const searchHandler = async () => {
   const url = VITE_APP_KAKAO_API_URI;
   const headers = {
     Authorization: `KakaoAK ${VITE_APP_KAKAO_REST_KEY}`,
@@ -15,16 +20,32 @@ const searchHandeler = async () => {
   const response = await axios.get(url, { headers: headers, params: params });
   searchList.value = response.data.documents;
 };
+
+const selectHandler = (item) => {
+  const isNotExist = !selectList.value.includes(item);
+  if (isNotExist) selectList.value = [...selectList.value, item];
+};
 </script>
 
 <template>
   <div>place location</div>
-  <form action="" @submit.prevent="" @submit="searchHandeler">
+  <form @submit.prevent="" @submit="searchHandler">
+    <p>가고 싶은 장소를 검색해주세요.</p>
+    <p>-> 검색 결과에서 원하는 장소를 선택해주세요</p>
     <input v-model="searchKeyword" />
     <button>검색</button>
   </form>
   <ul>
-    <div v-for="item in searchList" :key="item.id">
+    <div v-for="item in searchList" :key="item.id" @click="selectHandler(item)">
+      <li>{{ item.place_name }}</li>
+      <li>{{ item.address_name }}</li>
+      <br />
+    </div>
+  </ul>
+  <VKakaoMap :stations="selectList" :selectStation="selectPlace"></VKakaoMap>
+  <h3>선택한 장소 목록</h3>
+  <ul>
+    <div v-for="item in selectList" :key="item.id">
       <li>{{ item.place_name }}</li>
       <li>{{ item.address_name }}</li>
       <br />
