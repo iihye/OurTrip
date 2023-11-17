b7f91b8548e5e032baa63b89d22f1c755d49b558
이혜진 <floral_berry@naver.com>
2023-11-17 14:36:38 +0900

    feat: detail listno 불러오기

---
+++ FE/ourtrip/src/components/list/ListDetail.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const listno = ref(route.params.listno);
+
+onMounted(() => {
+  getList();
+});
+
+const getList = () => {
+  //   console.log('getList를 실행하였습니다');
+};
+</script>
+
+<template>
+  <div>List detail</div>
+  <span>{{ listno }}</span>
+</template>
+
+<style scoped></style>
--- FE/ourtrip/src/components/list/ListOpen.vue
+++ FE/ourtrip/src/components/list/ListOpen.vue
@@ -3,6 +3,8 @@ import { onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useListStore } from '@/stores/list';
 
+import ListItem from '@/components/list/ListItem.vue';
+
 const listStore = useListStore();
 
 const { openListRes } = storeToRefs(listStore);
@@ -15,7 +17,7 @@ onMounted(() => {
 const open = async () => {
   await openList();
   if (openListRes != []) {
-    console.log(openListRes);
+    // console.log(openListRes);
   } else {
     console.log('아무것도 없다!!!');
   }
@@ -25,9 +27,13 @@ const open = async () => {
 <template>
   <div>List open</div>
   <div v-if="openListRes.length === 0">비어있어요</div>
-  <template v-for="list in openListRes" :key="list.listno">
-    <li>{{ list.listName }}</li>
-  </template>
+  <ListItem v-for="list in openListRes" :key="list.listNo" :list="list"></ListItem>
+  <!-- <template v-for="list in openListRes" :key="list.listno">
+    <router-link :to="{ name: 'list-detail', params: { listNo: list.listNo } }">
+      {{ list.listName }}
+    </router-link>
+    <br />
+  </template> -->
 </template>
 
 <style scoped></style>
