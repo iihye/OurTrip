93aec62afa7841f9e2b1a7c3babc55e9177de25b
이혜진 <floral_berry@naver.com>
2023-11-17 13:54:22 +0900

    feat: detail BE 완성

--- BE/ourtrip/src/main/java/edu/ssafy/ourtrip/list/service/ListService.java
+++ BE/ourtrip/src/main/java/edu/ssafy/ourtrip/list/service/ListService.java
@@ -4,8 +4,10 @@ import java.sql.SQLException;
 import java.util.List;
 
 import edu.ssafy.ourtrip.list.dto.ListDto;
+import edu.ssafy.ourtrip.place.dto.PlaceDto;
 
 public interface ListService {
+	List<PlaceDto> detail(String listNo) throws SQLException;
 	List<ListDto> myList(String userId) throws SQLException;
 	List<ListDto> shareList(String userId) throws SQLException;
 	List<ListDto> openList() throws SQLException;
