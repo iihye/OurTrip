67beb4b45c1e5da8a09ac57a82738456dc7d988e
Lee Hye Jin <floral_berry@naver.com>
2023-11-18 03:18:00 +0900

    feat: main, nav, footer 임시 저장

--- FE/ourtrip/src/components/common/Nav.vue
+++ FE/ourtrip/src/components/common/Nav.vue
@@ -18,6 +18,41 @@ const logout = () => {
 </script>
 
 <template>
+  <nav class="navbar">
+    <div class="navbar__logo">
+        <i class="fab fa-accusoft"></i>
+        <router-link to="/">OurTrip</router-link>
+    </div>
+
+    <ul class="navbar__menu">
+      <li class="nav-item">
+        <router-link :to="{ name: 'place' }" class="nav-link">PLACELIST 만들기</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link :to="{ name: 'list-open' }" class="nav-link">공개 PLACELIST</router-link>
+      </li>
+            
+        <!-- <li><a href="">Home</a></li> -->
+      <template v-for="menu in menuList" :key="menu.routeName">
+        <template v-if="menu.show">
+          <template v-if="menu.routeName === 'user-logout'">
+            <li class="nav-item">
+              <router-link to="/" @click.prevent="logout" class="nav-link">{{ menu.name }}</router-link>
+            </li>
+          </template>
+
+          <template v-else>
+            <li class="nav-item">
+              <router-link :to="{ name: menu.routeName }" class="nav-link">{{ menu.name }}</router-link>
+            </li>
+          </template>
+        </template>
+      </template>
+    </ul>
+  </nav>
+
+
   <nav>
     <div class="container" style="width: 100%">
       <div id="navb">
@@ -71,4 +106,113 @@ const logout = () => {
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+:root{
+    --text-color: #f0f4f5;
+    --background-color: #868686;
+    --accent-color:#91684b;
+}
+
+body {
+    margin: 0px;
+    font-family: 'Open sans';
+}
+
+a {
+    text-decoration: none;
+    color: white;
+}
+
+.navbar {
+    position: fixed;
+    top: 0;
+    /* width: 100% */
+    left: 0;
+    right: 0;
+    display: flex;
+    z-index: 100;
+    justify-content: space-between;
+    align-items: center;
+    background:#5d5d5d;
+    padding: 8px 12px;
+}
+
+.navbar__logo {
+    font-size: 24px;
+    color: white;
+}
+
+.navbar__logo i {
+    color: #d49466;
+}
+
+.navbar__menu {
+    display: flex;
+    list-style: none;
+    padding-left: 0px;
+}
+
+.navbar__menu li {
+    padding: 8px 12px;
+
+}
+
+.navbar__menu :hover {
+    
+    border-radius: 4px;
+}
+
+.navbar__icons {
+    list-style: none;
+    color: white;
+    display: flex;
+    padding-left: 0px;
+}
+
+.navbar__icons li {
+    padding: 8px 12px;
+}
+
+.navbar__toogleBtn {
+    display: none;
+    position: absolute;
+    right: 32px;
+    font-size: 24px;
+    color: #d49466;
+}
+
+@media screen and (max-width: 768px) {
+    .navbar {
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 8px 24px;
+    }
+    
+    .navbar__menu {
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+    }
+
+    .navbar__menu li {
+        width: 100%;
+        text-align: center;
+    }
+
+    .navbar__icons {
+        display: none;
+        justify-content: center;
+        width: 100%;
+    }
+
+    .navbar__toogleBtn {
+        display: block;
+    }
+
+    .navbar__menu.active,
+    .navbar__icons.active {
+        display: flex;
+    }
+}
+</style>
--- FE/ourtrip/src/views/HomeView.vue
+++ FE/ourtrip/src/views/HomeView.vue
@@ -1,64 +1,186 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <div>home</div>
   <!--container 1-->
-  <container>
-    <div id="main1">
-      <h1>
+  <container id="super-container">
+    <div>
+      <h1 id="super-text">
         나만의 PLACE를 찾아 <br />
         PLACELIST를 만들어보세요!
       </h1>
-      <img id="main-img1" src="@/assets/img/main1.png" alt="main-img1" width="700" />
+    </div>
+    <div id="footer-btn-container">
+      <v-btn size="x-large" variant="outlined" rounded="xl" width="400"> 시작하기 </v-btn>
+    </div>
+    <div id="super-img1" >
+      <img src="@/assets/img/main1.png" alt="main-img1" width="700" />
     </div>
   </container>
 
   <!--container 2-->
-  <container>
-    <div id="sub-text">
-      <h1>
-        PLACE를 골라 <br />
-        PLACELIST 속으로!
-      </h1>
-      <v-btn variant="outlined" rounded="xl"> 시작하기 </v-btn>
+  <container id="main-container">
+    <div id="main-flex-container">
+      <div id="sub-container">
+        <h1 id="sub-title">
+          검색
+        </h1>
+        <h2 id="sub-content">
+          검색으로 찾는, <br/>
+          나만의 PLACE
+        </h2>
+      </div>
+      <div id="sub-img-container">
+        <img src="@/assets/img/main2.png" alt="main-img2" width="400" />
+      </div>
     </div>
+  </container>
 
-    <v-card id="main-card" class="mx-auto" max-width="400">
-      <v-card-item>
-        <div class="mb-1">
-          <h1>
-            검색으로 찾는 <br />
-            나만의 플레이스
-          </h1>
-          <img id="main-img1" src="@/assets/img/main2.png" alt="main-img1" width="300" />
-        </div>
-      </v-card-item>
-    </v-card>
+  <container id="main-container">
+    <div id="main-flex-container">
+      <div id="sub-container">
+        <h1 id="sub-title">
+          저장
+        </h1>
+        <h2 id="sub-content">
+          알아보기 쉽게, <br/>
+          어울리는 커버 사진 찾기
+        </h2>
+      </div>
+      <div id="sub-img-container">
+        <img src="@/assets/img/main3.png" alt="main-img3" width="400" />
+      </div>
+    </div>
+  </container>
+
+  <container id="main-container">
+    <div id="main-flex-container">
+      <div id="sub-container">
+        <h1 id="sub-title">
+          공유
+        </h1>
+        <h2 id="sub-content">
+          오직 친구에게만, <br/>
+          나만의 PLACELIST 보여주기
+        </h2>
+      </div>
+      <div id="sub-img-container">
+        <img src="@/assets/img/main4.png" alt="main-img4" width="400" />
+      </div>
+    </div>
+  </container>
+
+
+  <!--container 3!-->
+  <container id="foot-container">
+    <div>
+      <h1 id="foot-text">
+        지금 바로, <br />
+        PLACELIST를 만들어보세요!
+      </h1>
+    </div>
   </container>
 </template>
 
 <style scoped>
-.container {
+#super-container {
   width: 70%;
   height: 200px;
   overflow: hidden;
-}
-.mb-1 h1 {
-  line-height: 1.4;
-}
-#main1 {
   text-align: center;
   padding: 30px;
 }
-#main-img1 {
+#super-text{
+  width: 100%;
+  text-align: center;
+  font-size: 66px;
+  line-height: 1.4;
+  color: black;
+}
+#super-img1 {
   max-width: 100%;
   height: auto !important;
   padding: 10px;
 }
-#main-card {
-  border: 2px solid #000;
-  border-radius: 30px;
-  padding: 20px;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+#main-btn{
+  text-align: center; /* 텍스트를 수평 가운데 정렬합니다. */
+    width: 150px;
+    font-size: 16px;
+    margin: 0 auto; /* 가운데 정렬을 위한 마진 설정 */
 }
+#main-container {
+  display: flex;
+  justify-content: center; /* Optional: to center the content horizontally */
+  padding: 100px;
+}
+
+#main-flex-container {
+  display: flex;
+  width: 100%; 
+}
+
+#sub-container {
+  padding-left: 10%;
+  flex: 1; /* Takes up remaining space on the left */
+}
+
+#sub-img-container {
+  padding-right: 10%;
+  flex: 0 0 300px; /* Fixed width of 300px on the right */
+}
+#sub-title{
+  width: 100%;
+  text-align: left;
+  font-size: 28px;
+  line-height: 1.4;
+  color: mediumpurple;
+}
+#sub-content{
+  width: 100%;
+  text-align: left;
+  margin-bottom: 50px;
+  font-size: 50px;
+  line-height: 1.4;
+  color: black;
+}
+#foot-container {
+  width: 70%;
+  height: 200px;
+  overflow: hidden;
+  text-align: center;
+  padding: 30px;
+  margin-bottom: 200px;
+}
+#foot-text{
+  width: 100%;
+  text-align: center;
+  padding-top: 100px;
+  padding : 100px;
+  font-size: 66px;
+  line-height: 1.4;
+  color: black;
+}
+#footer-btn-container {
+  padding: 10px;
+  margin : 2%;
+  flex: 0 0 300px; /* Fixed width of 300px on the right */
+}
+
+/* #sub-container{
+  padding:200px;
+  flex: 1;
+}
+#sub-container2{
+  background-color: rgb(234, 225, 255);
+  padding:200px;
+  flex: 1;
+}
+
+#sub-img{
+  flex: 0 0 300px;
+}
+#main2{
+  padding:100px;
+  display: flex;
+} */
 </style>
