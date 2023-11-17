60cc69a79d9105056b574ba23b133c32249ef453
이혜진 <floral_berry@naver.com>
2023-11-17 13:47:06 +0900

    feat: place dto 작성

--- BE/ourtrip/src/main/java/edu/ssafy/ourtrip/place/dto/PlaceDto.java
+++ BE/ourtrip/src/main/java/edu/ssafy/ourtrip/place/dto/PlaceDto.java
@@ -1,5 +1,25 @@
 package edu.ssafy.ourtrip.place.dto;
 
-public class PlaceDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import java.math.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PlaceDto {
+	private int placeNo;
+	private String placeName;
+	private String placeUrl;
+	private String placeAddressName;
+	private String placeRoadAddressName;
+	private String placePhone;
+	private BigDecimal placeX;
+	private BigDecimal placeY;
+	private int listNo;
 }
