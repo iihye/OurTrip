26c14d1eea5e972b6c4b454c13d2142fc7b98c9e
이혜진 <floral_berry@naver.com>
2023-11-17 13:21:28 +0900

    feat: 전역 폰트 적용

--- FE/ourtrip/src/main.js
+++ FE/ourtrip/src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import './assets/base.css';
 
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
 
 // Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const vuetify = createVuetify({
   components,
@@ -20,4 +22,4 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(vuetify);
-app.mount("#app");
+app.mount('#app');
