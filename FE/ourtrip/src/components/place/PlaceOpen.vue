0fae701f75eabc46ecb9b5ac05dfd176a9f7ebdb
cyberjam <jamsince1994@naver.com>
2023-11-18 19:32:30 +0900

    feat: [FE] list post 기능 완성

--- FE/ourtrip/src/components/place/PlaceOpen.vue
+++ FE/ourtrip/src/components/place/PlaceOpen.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { ref } from "vue";
 import { usePlaceStore } from "@/stores/place";
+import axios from "axios";
 import { storeToRefs } from "pinia";
 
 const placeStore = usePlaceStore();
+const { VITE_APP_SERVER_URI } = import.meta.env;
+
 const { listInfo } = storeToRefs(placeStore);
 
 const selectIsOpen = ref(false);
@@ -14,8 +17,29 @@ const openButtonHandler = () => {
 
 const saveButtonHandler = () => {
   listInfo.value = { ...listInfo.value, list_open: selectIsOpen.value };
-  console.log(listInfo.value);
-  // list-my
+  // console.log(listInfo.value);
+  const listNo = registerList();
+  registerPlace(listNo);
+};
+
+const registerList = async () => {
+  const list_info = listInfo.value;
+  const url = `${VITE_APP_SERVER_URI}/list/register`;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const data = {
+    listName: list_info.list_name,
+    listImg: list_info.list_img,
+    listOpen: list_info.list_open,
+    userId: "test",
+  };
+
+  const response = await axios.post(url, data, headers);
+  return response;
+};
+const registerPlace = (listNo) => {
+  console.log(listNo);
 };
 </script>
 
