e7f2878ae3976eab744e96d477501804ba2d4b10
cyberjam <jamsince1994@naver.com>
2023-11-21 02:52:25 +0900

    feat: 인포윈도우 닫기 기능 완성

--- FE/ourtrip/src/components/common/VKakaoMap.vue
+++ FE/ourtrip/src/components/common/VKakaoMap.vue
@@ -1,18 +1,19 @@
 <script setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted } from 'vue';
 
 const { VITE_APP_KAKAO_JS_KEY } = import.meta.env;
 var map;
 const positions = ref([]);
 const markers = ref([]);
+const overlays = ref([]);
 
 const props = defineProps({ stations: Array, selectStation: Object });
 
 watch(
   () => props.stations.value,
   () => {
-    console.log("1");
     positions.value = [];
+    initMap();
     props.stations.forEach((station) => {
       let obj = {};
       obj.latlng = new kakao.maps.LatLng(station.y, station.x);
@@ -29,10 +30,7 @@ watch(
   () => props.selectStation.value,
   () => {
     // 이동할 위도 경도 위치를 생성합니다
-    var moveLatLon = new kakao.maps.LatLng(
-      props.selectStation.lat,
-      props.selectStation.lng
-    );
+    var moveLatLon = new kakao.maps.LatLng(props.selectStation.lat, props.selectStation.lng);
 
     // 지도 중심을 부드럽게 이동시킵니다
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
@@ -45,7 +43,7 @@ onMounted(() => {
   if (window.kakao && window.kakao.maps) {
     initMap();
   } else {
-    const script = document.createElement("script");
+    const script = document.createElement('script');
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${VITE_APP_KAKAO_JS_KEY}&libraries=services,clusterer`;
     /* global kakao */
     script.onload = () => kakao.maps.load(() => initMap());
@@ -54,10 +52,10 @@ onMounted(() => {
 });
 
 const initMap = () => {
-  const container = document.getElementById("map");
+  const container = document.getElementById('map');
   const options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
+    center: new kakao.maps.LatLng(36.3398, 127.394),
+    level: 7,
   };
   map = new kakao.maps.Map(container, options);
 
@@ -76,61 +74,40 @@ const loadMarkers = () => {
 
   // 마커를 생성합니다
   markers.value = [];
-  console.log(positions);
   positions.value.forEach((position) => {
     const marker = new kakao.maps.Marker({
-      map: map, // 마커를 표시할 지도
-      position: position.latlng, // 마커를 표시할 위치
-      title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨.
-      clickable: true, // // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+      map: map,
+      position: position.latlng,
+      title: position.title,
+      clickable: true,
       // image: markerImage, // 마커의 이미지
     });
     markers.value.push(marker);
 
-    // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-    kakao.maps.event.addListener(marker, "click", function () {
-      // 커스텀 오버레이에 표시할 컨텐츠 입니다
-      // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
-      // 별도의 이벤트 메소드를 제공하지 않습니다
-
-      let content = `<div class="wrap">
-            <div class="info">
-                <div class="title">
-                    ${position.title}
-                    <div class="close" onclick="${closeOverlay()}" title="닫기"></div>
-                </div>" 
-                <div class="body">
-                    <div class="img">
-                        <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">
-                   </div>
-                    <div class="desc">
-                        <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>
-                        <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>
-                        <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-
-      // 마커 위에 커스텀오버레이를 표시합니다
-      // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
-      const overlay = new kakao.maps.CustomOverlay({
-        content: content,
+    kakao.maps.event.addListener(marker, 'click', function () {
+      new kakao.maps.InfoWindow({
+        content: `<div class="wrap">
+                  <div class="info">
+                      <div class="title">
+                          ${position.title}
+                      </div>"
+                      <div class="body">
+                          <div class="img">
+                              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">
+                        </div>
+                          <div class="desc">
+                              <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>
+                              <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>
+                              <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>
+                          </div>
+                      </div>
+                  </div>
+              </div>`,
         map: map,
         position: marker.getPosition(),
+        removable: true,
       });
-      overlay.setMap(map);
-
-      // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
-      function closeOverlay() {
-        overlay.setMap(null);
-      }
     });
-    // 마커에 클릭이벤트를 등록합니다
-    // kakao.maps.event.addListener(marker, "click", function () {
-    //   // 마커 위에 인포윈도우를 표시합니다
-    //   console.log("click");
-    // });
   });
 
   // 4. 지도를 이동시켜주기
@@ -167,11 +144,12 @@ const deleteMarkers = () => {
   bottom: 40px;
   width: 288px;
   height: 132px;
-  margin-left: -144px;
+  margin-left: -133px;
+  margin-bottom: -78px;
   text-align: left;
   overflow: hidden;
   font-size: 12px;
-  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;
   line-height: 1.5;
 }
 .wrap * {
@@ -189,7 +167,7 @@ const deleteMarkers = () => {
 }
 .wrap .info:nth-child(1) {
   border: 0;
-  box-shadow: 0px 1px 2px #888;
+  box-shadow: 0px 3px 10px #888;
 }
 .info .title {
   padding: 5px 0 0 10px;
@@ -199,26 +177,15 @@ const deleteMarkers = () => {
   font-size: 18px;
   font-weight: bold;
 }
-.info .close {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  color: #888;
-  width: 17px;
-  height: 17px;
-  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png");
-}
-.info .close:hover {
-  cursor: pointer;
-}
+
 .info .body {
   position: relative;
   overflow: hidden;
 }
 .info .desc {
   position: relative;
-  margin: 13px 0 0 90px;
-  height: 75px;
+  margin: 0px 0 0 90px;
+  height: 90px;
 }
 .desc .ellipsis {
   overflow: hidden;
@@ -232,7 +199,7 @@ const deleteMarkers = () => {
 }
 .info .img {
   position: absolute;
-  top: 6px;
+  top: 0px;
   left: 5px;
   width: 73px;
   height: 71px;
@@ -241,14 +208,14 @@ const deleteMarkers = () => {
   overflow: hidden;
 }
 .info:after {
-  content: "";
+  content: '';
   position: absolute;
-  margin-left: -12px;
+  margin-left: 53px;
   left: 50%;
   bottom: 0;
   width: 22px;
   height: 12px;
-  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+  background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png');
 }
 .info .link {
   color: #5085bb;
