11002466558c129c4f81ec1cbb3050a4fd60dc25
이혜진 <floral_berry@naver.com>
2023-11-20 09:22:14 +0900

    feat: footer 추가

--- FE/ourtrip/src/App.vue
+++ FE/ourtrip/src/App.vue
@@ -1,26 +1,39 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 import Nav from './components/common/Nav.vue';
-
+import Footer from './components/common/Footer.vue';
 </script>
 
 <template>
   <div id="wrapper">
     <Nav />
-    <RouterView />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div>
+      <RouterView />
+    </div>
   </div>
-  
-  
+
   <!-- <footer id="footer">
     <h1>footer</h1>
   </footer> -->
-  <!-- <Footer id="footer"></Footer> -->
+  <Footer id="footer"></Footer>
 </template>
 
 <style scoped>
-#app {
+#wrapper {
+  height: auto;
   min-height: 100%;
   position: relative;
-  padding-bottom: 170px;
+  padding-bottom: 150px;
+}
+Footer {
+  height: 150px;
+  position: relative;
+  transform: translateY(0%);
+  display: flex;
 }
 </style>
