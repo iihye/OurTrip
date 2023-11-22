4c0a97dbf1644c490635cd4f85bd3a8d077f865c
이혜진 <floral_berry@naver.com>
2023-11-22 09:52:54 +0900

    feat: reply BE 완성

---
+++ BE/ourtrip/src/main/java/edu/ssafy/ourtrip/reply/controller/ReplyController.java
@@ -0,0 +1,124 @@
+package edu.ssafy.ourtrip.reply.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ssafy.ourtrip.reply.dto.ReplyDto;
+import edu.ssafy.ourtrip.reply.service.ReplyService;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/reply")
+@Slf4j
+public class ReplyController {
+	private ReplyService replyService;
+	
+	public ReplyController(ReplyService replyService) {
+		this.replyService = replyService;
+	}
+	
+	@PostMapping("/regist")
+	public ResponseEntity<Map<String, Object>> join(@RequestBody ReplyDto replyDto){
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			replyService.regist(replyDto);
+			resultMap.put("message", "등록 성공");
+			status = HttpStatus.CREATED;
+		} catch(Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@DeleteMapping("/delete/{replyNo}")
+	public ResponseEntity<Map<String, Object>> delete(@PathVariable("replyNo") int replyNo){
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			replyService.delete(replyNo);
+			resultMap.put("message", "삭제 성공");
+			status = HttpStatus.CREATED;
+		} catch(Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	} 
+	
+	@PostMapping("/addLike/{replyNo}")
+	public ResponseEntity<Map<String, Object>> addLike(@PathVariable("replyNo") int replyNo){
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			int cnt = replyService.addLike(replyNo);
+			if(cnt > 0) {
+				resultMap.put("message", "좋아요 증가 성공");
+				status = HttpStatus.OK;
+			}
+		} catch(Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@PostMapping("/subLike/{replyNo}")
+	public ResponseEntity<Map<String, Object>> subLike(@PathVariable("replyNo") int replyNo){
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			int cnt = replyService.subLike(replyNo);
+			if(cnt > 0) {
+				resultMap.put("message", "좋아요 감소 성공");
+				status = HttpStatus.OK;
+			}
+		} catch(Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@GetMapping("/getLike/{replyNo}")
+	public ResponseEntity<Map<String, Object>> getLike(@PathVariable("replyNo") int replyNo){
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			int cnt = replyService.getLike(replyNo);
+			resultMap.put("replyLike", cnt);
+			status = HttpStatus.CREATED;
+		} catch(Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+	
+	@GetMapping("/getReply/{listNo}")
+	public ResponseEntity<Map<String, Object>> getReply(@PathVariable("listNo") int listNo){
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		HttpStatus status = HttpStatus.ACCEPTED;
+		try {
+			List<ReplyDto> list= replyService.getReply(listNo);
+			resultMap.put("list", list);
+			status = HttpStatus.CREATED;
+		} catch(Exception e) {
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+}
---
+++ BE/ourtrip/src/main/java/edu/ssafy/ourtrip/reply/dao/ReplyMapper.java
@@ -0,0 +1,18 @@
+package edu.ssafy.ourtrip.reply.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.ssafy.ourtrip.reply.dto.ReplyDto;
+
+@Mapper
+public interface ReplyMapper {
+	void regist(ReplyDto replyDto) throws SQLException;
+	void delete(int replyNo) throws SQLException;
+	int addLike(int replyNo) throws SQLException;
+	int subLike(int replyNo) throws SQLException;
+	int getLike(int replyNo) throws SQLException;
+	List<ReplyDto> getReply(int listNo) throws SQLException;
+}
