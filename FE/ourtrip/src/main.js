97a92815829008c169e5bea07129501394e6ce70
cyberjam <jamsince1994@naver.com>
2023-11-24 04:22:48 +0900

    refactor: 컴포넌트 이름 상세하게 변경

--- FE/ourtrip/src/main.js
+++ FE/ourtrip/src/main.js
@@ -16,7 +16,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import SvgIcon from "./components/common/icon.vue";
+import SvgIcon from "./components/common/SvgIcon.vue";
 
 library.add(fas);
 library.add(far);
