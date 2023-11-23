4b2d4c8bc7b1835641b455de34705937813e43aa
cyberjam <jamsince1994@naver.com>
2023-11-24 04:20:36 +0900

    feat: svg component 작성

---
+++ FE/ourtrip/src/components/common/icon.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+// feel free to update this with an svg directory of your choice
+const icon = defineAsyncComponent(() => import(`/src/assets/img/${props.name}.svg`));
+</script>
+
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
